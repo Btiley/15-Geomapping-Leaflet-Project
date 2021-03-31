fd223b29aa86b538c52ae5cfe9b5015acb28a629
Brandon Tiley <bctiley@gmail.com>
2021-03-31 15:42:19 +1100

    Created refrence to all marker params, added initial comments

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,13 +1,19 @@
+//CREATE INITAL MAP FUNCTION
+//----------------------------------------------------
+
+//Map Params (Centred on West Coast USA)
+var mapCentre = [38.575764, -121.478851]
+var mapZoom = 5
 
 function createMap(){
-console.log("Map has Run");
 // Initialize map object
 var myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 13
+    center: mapCentre,
+    zoom: mapZoom
   });
   
 //Add 'streetmap' tile layer
+
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -20,11 +26,38 @@ var myMap = L.map("map", {
 
 createMap() 
 
+//IMPORT DATA
+//----------------------------------------------------
+
 //Get JSON and scrape coords of earthquake.
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 d3.json(url, function(response) {
     var lng = response["features"][0]["geometry"]["coordinates"][0];
     var lat = response["features"][0]["geometry"]["coordinates"][1];
-    console.log(`The coords are long: ${lng}, lat: ${lat}`);
+    var mag = response["features"][0]["properties"]["mag"];
+    var loc = response["features"][0]["properties"]["place"];
+
+    console.log(`The Earthquake occured ${loc} (${lng},${lat}) and had a magnitude of ${mag}`);
+
+//Create Markers, Tooltips and Legend. 
+//----------------------------------------------------
+
+//MARKERS
+//----------------------------------------------------
+
+//Circle markers with radius = mag, gradient colour, darker = higher mag.
+
+//TOOL TIP
+//----------------------------------------------------
+
+//Tool tips on Mouse over with local, lat/long and magnitude.
+
+//LEGEND
+//----------------------------------------------------
+
+//Legend to show gradient of magnitudes and values.
+
+
 });
+
